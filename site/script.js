a3ab993cc664c4e3d78b7aa6a9ae94496683ff11
pranjal-arora <prv.gma@gmail.com>
2021-10-18 02:14:42 +0530

    CSS and JS files separated, refers to issue #10

---
+++ site/script.js
@@ -0,0 +1,24 @@
+$(function(){
+    $("#navbar").load("nav.html");
+  });
+
+$(function(){
+    $("#carousel").load("carousel.html");
+  });
+
+$(function(){
+    $("#1989").load("1989.html");
+  });
+
+$(function(){
+    $("#lover").load("lover.html");
+  });
+  
+$(function(){
+    $("#wall").load("wall.html");
+  });
+
+$(function(){
+    $("#footer").load("footer.html");
+  });
+
